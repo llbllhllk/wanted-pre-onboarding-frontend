d940cbb6e9b30dce82d9e93bc782c81a15ad9e30
llbllhllk <rkdqudgus9806@gmail.com>
2023-08-04 17:38:47 +0900

    Feat: TodoList, TodoListItem 컴포넌트 추가

---
+++ src/components/TodoList.js
@@ -0,0 +1,5 @@
+const TodoList = () => {
+  return <ol></ol>;
+};
+
+export default TodoList;
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,12 @@
+const TodoListItem = () => {
+  return (
+    <li>
+      <label>
+        <input type="checkbox" />
+        <span>TODO 1</span>
+      </label>
+    </li>
+  );
+};
+
+export default TodoListItem;
